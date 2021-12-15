497001b8c7d7675f95b88b81ccef205b481a6f74
YASH TEWATIA <yashbsr3@gmail.com>
2021-12-15 23:08:59 +0530

    added actual response

--- auth/views/profile.py
+++ auth/views/profile.py
@@ -24,10 +24,10 @@ def detect(request):
 def response(request,emotion):
     now = datetime.now()
     now_time = now.time()
-    response_path = emotion+".wav"
+    extension = ".wav"
+    response_path = emotion
     data_body , data_link = "",""
     if now_time >= time(21,00) or now_time <= time(8,00):
-        response_path = "response/night_" + response_path
         if emotion == "happy":
             data_body = "Listen Songs at"
             data_link = "https://www.spotify.com/us/"
@@ -35,12 +35,14 @@ def response(request,emotion):
             data_body = "Wanna listen to some sleep playlist at"
             data_link = "https://www.spotify.com/us/"
         elif emotion == "angry":
+            extension = ".mp3"
             data_body = "Wanna listen to some calm playlist at"
             data_link = "https://www.spotify.com/us/"
         elif emotion == "surprise":
             data_body = "Share your excitement on Social Media"
             data_link = "https://www.facebook.com/"
         elif emotion == "disgust":
+            extension = ".mp3"
             data_body = "Watch something on Youtube to get diverted"
             data_link = "https://www.youtube.com/"
         elif emotion == "fear":
@@ -49,8 +51,8 @@ def response(request,emotion):
         else:
             data_body = "Wanna listen to some sleep playlist at"
             data_link = "https://www.spotify.com/us/"
+        response_path = "response/night_" + response_path + extension
     else:
-        response_path = "response/day_" + response_path
         if emotion == "happy":
             data_body = "let's netflix and chill"
             data_link = "https://www.netflix.com"
@@ -58,6 +60,7 @@ def response(request,emotion):
             data_body = "You can connect to someone on social media"
             data_link = "https://www.facebook.com/"
         elif emotion == "angry":
+            extension = ".mp3"
             data_body = "Wanna listen to some calm playlist at"
             data_link = "https://www.spotify.com/us/"
         elif emotion == "surprise":
@@ -72,6 +75,7 @@ def response(request,emotion):
         else:
             data_body = "Wanna order some food online"
             data_link = "https://www.zomato.com"
+        response_path = "response/day_" + response_path + extension
     data = render_to_string("data.html",{"data_body": data_body,"data_link": data_link,})
     return render(request,"auth/response.html", {"emotion": emotion,"resp":response_path,"data":data})
     
