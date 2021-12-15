3d54bbe9bde54c5905f62378e26cc7b3d3a812b3
YASH TEWATIA <yashbsr3@gmail.com>
2021-12-15 19:27:33 +0530

    for hosting

--- auth/views/profile.py
+++ auth/views/profile.py
@@ -25,11 +25,54 @@ def response(request,emotion):
     now = datetime.now()
     now_time = now.time()
     response_path = emotion+".wav"
-    data = render_to_string("data.html",{"data_body": "Listen Songs at","data_link": "https://www.spotify.com/us/",})
+    data_body , data_link = "",""
     if now_time >= time(21,00) or now_time <= time(8,00):
         response_path = "response/night_" + response_path
+        if emotion == "happy":
+            data_body = "Listen Songs at"
+            data_link = "https://www.spotify.com/us/"
+        elif emotion == "sad":
+            data_body = "Wanna listen to some sleep playlist at"
+            data_link = "https://www.spotify.com/us/"
+        elif emotion == "angry":
+            data_body = "Wanna listen to some calm playlist at"
+            data_link = "https://www.spotify.com/us/"
+        elif emotion == "surprise":
+            data_body = "Share your excitement on Social Media"
+            data_link = "https://www.facebook.com/"
+        elif emotion == "disgust":
+            data_body = "Watch something on Youtube to get diverted"
+            data_link = "https://www.youtube.com/"
+        elif emotion == "fear":
+            data_body = "Wanna listen to some sleep playlist at"
+            data_link = "https://www.spotify.com/us/"
+        else:
+            data_body = "Wanna listen to some sleep playlist at"
+            data_link = "https://www.spotify.com/us/"
     else:
         response_path = "response/day_" + response_path
+        if emotion == "happy":
+            data_body = "let's netflix and chill"
+            data_link = "https://www.netflix.com"
+        elif emotion == "sad":
+            data_body = "You can connect to someone on social media"
+            data_link = "https://www.facebook.com/"
+        elif emotion == "angry":
+            data_body = "Wanna listen to some calm playlist at"
+            data_link = "https://www.spotify.com/us/"
+        elif emotion == "surprise":
+            data_body = "Wanna order some food online"
+            data_link = "https://www.zomato.com"
+        elif emotion == "disgust":
+            data_body = "Watch something on Youtube to get diverted"
+            data_link = "https://www.youtube.com/"
+        elif emotion == "fear":
+            data_body = "Watch some Yoga videos and calm down"
+            data_link = "https://www.youtube.com/"
+        else:
+            data_body = "Wanna order some food online"
+            data_link = "https://www.zomato.com"
+    data = render_to_string("data.html",{"data_body": data_body,"data_link": data_link,})
     return render(request,"auth/response.html", {"emotion": emotion,"resp":response_path,"data":data})
     
 
