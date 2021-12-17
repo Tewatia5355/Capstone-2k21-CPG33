ab4920d8acf94b86d3681f6ec387931d2838ca39
Yash Kumar <60055574+Tewatia5355@users.noreply.github.com>
2021-12-17 09:59:37 +0530

    added forgot pass urls

--- auth/urls.py
+++ auth/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path("response/<slug:emotion>", views.response, name="response"),
     path("profile/", views.prof, name="profile"),
     path("activate/<uid64>/<token>", views.activate, name="activate"),
+    path("forget_pass_req", views.forget_pass_req, name="forget_pass_req"),
+    path("forget/<uid64>/<token>", views.forget_pass, name="forget"),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
