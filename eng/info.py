3da0148a65f8b664ade6735f41e945984cd3f928
Yash Kumar <60055574+Tewatia5355@users.noreply.github.com>
2021-12-21 14:33:18 +0530

    Update info.py

--- eng/info.py
+++ eng/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "capstone2k21.cpg33@gmail.com"
-EMAIL_HOST_PASSWORD = "alphabeta33"
+EMAIL_HOST =  ## enter email host, i.e. 'smtp.gmail.com'
+EMAIL_HOST_USER = ## enter email address
+EMAIL_HOST_PASSWORD = ## enter passwrod of email address
 EMAIL_PORT = 587
